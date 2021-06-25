aa99da07547180bb15e8124fb944079c29c27aef
lxueaa <kungfu.big.head@hotmail.com>
2021-06-25 16:06:02 +0800

    data preparation

---
+++ deeprapper/generate.sh
@@ -0,0 +1,30 @@
+python generate.py \
+                      --device '0' \
+                      --length 512 \
+                      --batch_size 1 \
+                      --nsamples 2 \
+                      --temperature 1 \
+                      --topk 8 \
+                      --topp 0 \
+                      --repetition_penalty 1. \
+                      --save_samples \
+                      --save_samples_dir 'samples_save_dir' \
+                      --samples_sign 'sample_name' \
+                      --model_dir 'model_path' \
+                      --model_config 'config.json' \
+                      --enable_final \
+                      --enable_sentence \
+                      --enable_relative_pos \
+                      --enable_beat \
+                      --prefix '我长大的地方像一个简朴的寨' \
+                      --with_beat \
+                      --beat_mode 0 \
+                      --reverse \
+                      --pattern 'beam' \
+                      --beam_sample_select_sg 'sample' \
+                      --beam_cut_temperature 10 \
+                      --dynamic_rhyme \
+                      --rhyme_count 1 \
+                      --rhyme_prob_bound 1.0 \
+                      --rhyme_alpha 0.95 \
+
