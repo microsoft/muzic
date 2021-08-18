6ce58f87cc7e6a2da7157b182d71eceb04be2e33
StillKeepTry <sktsxy@gmail.com>
2021-08-18 08:40:52 +0000

    update data generation

--- songmass/evaluate/calculate_ppl/l2m/5_delete_more.sh
+++
@@ -1,8 +0,0 @@
-data_dir=l2m
-for idx in `ls $data_dir`
-do
-	folder=$data_dir/$idx
-	rm $folder/dict.*
-	rm $folder/*.idx
-	rm $folder/*.bin
-done
--- songmass/evaluate/calculate_ppl/m2l/5_delete_more.sh
+++
@@ -1,9 +0,0 @@
-data_dir=m2l
-for idx in `ls $data_dir`
-do
-	folder=$data_dir/$idx
-	rm $folder/dict.*
-	rm $folder/*.idx
-	rm $folder/*.bin
-done
-
